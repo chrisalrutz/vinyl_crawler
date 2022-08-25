205468eb289e9e76c464cbef9628f50fff952078
Chris A <chris.alrutz@gmail.com>
2022-08-24 21:52:22 -0400

    debugged parsing errors

--- discogs_crawler/spiders/recordListings_py.py
+++ discogs_crawler/spiders/recordListings_py.py
@@ -1,10 +1,11 @@
 import scrapy
-
+from discogs_crawler.items import Listing
 
 class RecordlistingsPySpider(scrapy.Spider):
     name = 'recordListings.py'
     allowed_domains = ['www.discogs.com']
-    
+    listings = []
+
     def start_requests(self):
         urls = ['https://www.discogs.com/seller/philadelphiamusic/profile?format=Vinyl&genre=Electronic',
                 'https://www.discogs.com/seller/philadelphiamusic/profile?format=Vinyl&genre=Hip+Hop',
@@ -15,13 +16,25 @@ class RecordlistingsPySpider(scrapy.Spider):
 
 
     def parse(self, response):
+        listings = []
         for listing in response.xpath('//tr[@class="shortcut_navigable "]'):
-            title = response.xpath('//p[@class="item_description_title"]').get()
-            labelCat = response.xpath('//p[@class="label_and_cat"]').getall()
-            itemCatNo = response.xpath('//span[@class="label_and_cat"]').get()
-            sleeveCondition = response.xpath('//span[@class="item_sleeve_condition"]').get()
-            rUrl = response.xpath('//a[@class="item_release_link"]').get()
-            price = response.xpath('//span[@class="price"]').get()
+            yield{
+                'title' : response.xpath('//a[@class="item_description_title"]/text()').get(),
+                'label' : response.xpath('//p[@class="hide_mobile label_and_cat"]/a/text()').get(),
+                'itemCatNo' : response.xpath('//p[@class="hide_mobile label_and_cat"]/span[@class="item_catno"]/text()').get(),
+                'sleeveCondition' : response.xpath('//span[@class="item_sleeve_condition"]/text()').get(),
+                'Url' : response.xpath('//a[@class="item_release_link hide_mobile"]').attrib['href'],
+                'price' : response.xpath('//span[@class="price"]/text()').get()
+            }
+
+            # thisListing = Listing()
+            # thisListing['title'] = title
+            # thisListing['labelCat'] = labelCat
+            # thisListing['itemCatNo'] = itemCatNo
+            # thisListing['sleeveCondition'] = sleeveCondition
+            # thisListing['rUrl'] = rUrl
+            # thisListing['price'] = price
+            # listings.append(thisListing)
 
         next_page = response.xpath('//a[@class="pagination_next"]').attrib['href']
         if next_page is not None:
